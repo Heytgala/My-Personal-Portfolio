39589426170f7ec24e443464b4dac192377c9567
Heytgala <heytgala26@gmail.com>
2024-06-09 13:04:29 -0500

    Change in Grid concept

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -48,7 +48,7 @@ export const BentoGridItem = ({
     return (
         <div
             className={cn(
-                "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+                "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
                 className
             )}
             style={{ background: 'rgb(4,7,29)', backgroundColor: 'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%' }}
@@ -73,7 +73,12 @@ export const BentoGridItem = ({
                         {title}
                     </div>
                 </div>
-                {id===2 && <GlobeDemo /> }
+                {id === 2 && <GlobeDemo />}
+                {id === 3 && (<div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+                    <div className="flex flex-col gap-3 lg:gap-8">
+                        {['React.js', 'Next.js', 'TypeScript', 'Python', 'Java'].map((item) => (<span key={item } className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{ item}</span>)) }
+                    </div>
+                </div>)}
             </div>
         </div>
     );
