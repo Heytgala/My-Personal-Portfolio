52e592130cca637744e967f1bded3d0b229ccd99
Heytgala <heytgala26@gmail.com>
2025-01-31 15:36:29 -0600

    Updated Main Page

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -21,21 +21,21 @@ export const FloatingNav = ({
                 className
             )}
         >
-            <div className="flex items-center px-10 py-5">
-                <span className="text-white text-3xl unique-font">Heyt Gala</span>
+            <div className="flex items-center px-4 py-3 sm:px-6 sm:py-4 md:px-10 md:py-5">
+                <span className="text-white text-2xl sm:text-3xl md:text-4xl unique-font">Heyt Gala</span>
             </div>
-            <div className="flex items-center space-x-6 px-10 py-5">
-                {navItems.map((navItem: any, idx: number) => (
-                <Link
-                    key={`link=${idx}`}
-                    href={navItem.link}
-                    className={cn(
-                        "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
-                    )}
-                >
-                    <span className="block sm:hidden">{navItem.icon}</span>
-                    <span className="hidden sm:block text">{navItem.name}</span>
-                </Link>
+            <div className="flex items-center space-x-4 sm:space-x-6 px-4 py-3 sm:px-6 sm:py-4 md:px-10 md:py-5">
+                {navItems.map((navItem, idx) => (
+                    <Link
+                        key={`link=${idx}`}
+                        href={navItem.link}
+                        className={cn(
+                            "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+                        )}
+                    >
+                        <span className="block sm:hidden">{navItem.icon}</span>
+                        <span className="hidden sm:block text">{navItem.name}</span>
+                    </Link>
                 ))}
             </div>
         </div>
