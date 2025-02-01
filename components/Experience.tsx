611a1b8ff2926a134644abc5e31625f0112415cd
Heytgala <heytgala26@gmail.com>
2025-02-01 00:10:56 -0600

    Updated UI changes

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -14,11 +14,12 @@ const Experience = () => {
                         <div className="p-3 py-6 md:p-5 lg:p-10">
                             <div className="flex lg:flex-row flex-col lg:items-center gap-2">
                                 <img src={card.thumbnail} alt={card.thumbnail} className="lg:w-16 md:w-10 w-8" />
-                                <h1 className="text-start text-xl md:text-2xl font-bold">
+                                <h1 className="text-start text-2xl md:text-2xl font-bold">
                                     {card.title}
                                 </h1>
                             </div>
-                            <ul className="text-start text-white-100 mt-3 font-semibold list-disc list-inside">
+                            
+                            <ul className="text-start text-white-500 mt-3 font-semibold list-disc list-inside text-lg">
                                 {card.desc.map((point, index) => (
                                     <li key={index}>{point}</li>
                                 ))}
